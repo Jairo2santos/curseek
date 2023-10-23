23e570e4131d86791bace8f2e94761fbaae1e8c5
Matias Alejo Varas <mativaras71@gmail.com>
2023-10-23 18:22:38 -0300

    Update Hibridas: login/buscador basico

--- Client/src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import router from './router/index.js';
-
-createApp(App).use(router).mount('#app');
-
--- Client/src/views/home.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div>
-    <h1>Bienvenida a la página de inicio</h1>
-    
-  </div>
-</template>
