ee4573541911fa98666da0020a6cc7bcbfb7ea7f
Matias Alejo Varas <mativaras71@gmail.com>
2023-11-15 16:20:06 -0300

    Update home.vue

--- Client/src/views/home.vue
+++ Client/src/views/home.vue
@@ -1,33 +1,46 @@
 <template>
-  <div>
+  <div :class="{ 'bg-red-500': isInputClicked }" class="p-0 min-h-screen bg-indigo-500">
+    <!-- Banner -->
+    <div class="text-white text-center p-12">
+      <h1 class="text-2xl font-semibold">Encuentra tu curso</h1>
+    </div>
 
-    <div class="p-4 min-h-screen flex flex-col items-center bg-purple-500">
-      <div class="text-center mb-4"> <!-- Reducido el margen inferior -->
-        <h1 class="text-2xl font-extrabold text-white">Encuentra tu Curso</h1> <!-- Tamaño de fuente reducido -->
-      </div>
-
-      <div class="max-w-screen-xl mx-auto w-full flex items-center justify-center">
-        <!-- Campo de búsqueda con sombra y título alineado a la izquierda -->
-        <div class="bg-white w-full md:w-1/2 md:ml-0 md:mr-auto border rounded-xl">
-          <form @submit.prevent="search" class="flex items-center overflow-hidden shadow-md p-2">
-            <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24">
-              <path d="M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0-184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z"/>
-            </svg>
-            <input v-model="query" placeholder="Buscar cursos..." class="p-2 flex-grow outline-none" @focus="blurBackground" @blur="unblurBackground"/>
-          </form>
-        </div>
-
-
-        <!-- Imagen con enlace del lado derecho -->
-        <div class="w-2/3 md:w-1/3 md:ml-auto md:mr-0 mt-6">
-            <img class="w-full" src="../assets/similarweb.jpeg" alt="Never stop learning.">
-            <!-- <img class="w-full" src="https://ccweb.imgix.net/https%3A%2F%2Fwww.classcentral.com%2Fimages%2F%2Fillustrations%2Fhomepage-hero%2Fhero-halloween.png?auto=format&h=650&ixlib=php-4.1.0&s=88e0a1846e26c03246b7913d5f900bda" alt="Never stop learning."> -->
-        </div>
-      </div>
+    <!-- Campo de búsqueda -->
+    <div class="max-w-lg mx-auto pt-8 p-4">
+      <form @submit.prevent="search" class="flex p-1 bg-white items-center border rounded-xl overflow-hidden">
+        <button type="submit" class="p-2">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" width="24">
+            <path
+            d="M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z" />
+          </svg>
+        </button>
+        <input
+          v-model="query"
+          placeholder="Buscar..."
+          class="rounded-xl p-3 flex-grow outline-none"
+          @focus="setInputClicked(true)"
+          @blur="setInputClicked(false)"
+        />
+        <button v-if="query" type="button" @click="clearSearch" class="p-2">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="18">
+            <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z" />
+          </svg>
+        </button>
+      </form>
     </div>
 
     <!-- Resultados -->
-    <div class="mt-1 w-xl p-1"> <!-- Reducido el margen superior --></div>
+    <div class="p-5">
+      <!-- Aquí deberías tener tu componente CardCurso -->
+    </div>
+
+    <div class="flex flex-col md:flex-row container mx-auto md:px-40 p-0 bg-white mx-auto">
+      <img src="../assets/banner-1.jpg" alt="Descripción de la imagen" width="700" />
+      <p class="p-12">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium earum fugit molestiae in tenetur ad hic et
+        repellat aliquid voluptates quibusdam, , ullam sapiente, recusandae ab consectetur cupiditate porro quasi velit!
+      </p>
+    </div>
   </div>
 </template>
 
@@ -35,25 +48,33 @@
 import { ref } from 'vue';
 import axios from 'axios';
 
-    const query = ref('');
-    const courses = ref([]);
+const query = ref('');
+const courses = ref([]);
+const isInputClicked = ref(false);
 
-    const search = async () => {
-      try {
-        const response = await axios.get(`http://localhost:3333/cursos/search?q=${query.value}`);
-        courses.value = response.data;
-      } catch (error) {
-        console.error("Error al buscar cursos:", error);
-      }
-    };
+const search = async () => {
+  try {
+    const response = await axios.get(`http://localhost:3333/cursos/search?q=${query.value}`);
+    courses.value = response.data;
+  } catch (error) {
+    console.error("Error al buscar cursos:", error);
+  }
+};
 
-    const blurBackground = () => {
-      document.querySelector('.p-4').style.filter = 'blur(5px)';
-    };
+const blurBackground = () => {
+  document.querySelector('.p-4').style.filter = 'blur(5px)';
+};
 
-    const unblurBackground = () => {
-      document.querySelector('.p-4').style.filter = 'none';
-    };
+const unblurBackground = () => {
+  document.querySelector('.p-4').style.filter = 'none';
+};
 
+const clearSearch = () => {
+  query.value = '';
+  courses.value = [];
+};
 
+const setInputClicked = (clicked) => {
+  isInputClicked.value = clicked;
+};
 </script>
