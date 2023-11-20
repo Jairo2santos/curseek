1561a80f5b328be1aff5821f8efc68c675e80e18
Matias Alejo Varas <mativaras71@gmail.com>
2023-11-20 17:59:10 -0300

    Mayusculas categorias

--- Client/src/views/coursesViews/utn/detalleCursoUTN.vue
+++ Client/src/views/coursesViews/utn/detalleCursoUTN.vue
@@ -9,8 +9,6 @@
           <!-- Sección de Título, Duración y Precio -->
           <div class="bg-white p-4 md:p-6 rounded mb-4 justify-between items-center border border-gray-200">
             <h2 class="text-2xl md:text-3xl font-bold text-gray-800">{{ course.title }}</h2>
-            <!-- <h2 class="md:text-xl font-semibold text-green-500 py-1">{{ course.price }}</h2> -->
-            <!-- <p class="text-md py-1">{{ course.duration }}</p> -->
           </div>
 
           <!-- Sección de Resumen -->
@@ -49,13 +47,13 @@
             <ul>
               <!-- Categoría -->
               <li class="flex text-sm text-black p-2 border-b border-gray-100">
-                      <div class="flex items-center">
-                        <svg class="mr-2" fill="#6366f1" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" width="24" style="min-width: 24px;">
-                          <path d="M260-320q47 0 91.5 10.5T440-278v-394q-41-24-87-36t-93-12q-36 0-71.5 7T120-692v396q35-12 69.5-18t70.5-6Zm260 42q44-21 88.5-31.5T700-320q36 0 70.5 6t69.5 18v-396q-33-14-68.5-21t-71.5-7q-47 0-93 12t-87 36v394Zm-40 118q-48-38-104-59t-116-21q-42 0-82.5 11T100-198q-21 11-40.5-1T40-234v-482q0-11 5.5-21T62-752q46-24 96-36t102-12q58 0 113.5 15T480-740q51-30 106.5-45T700-800q52 0 102 12t96 36q11 5 16.5 15t5.5 21v482q0 23-19.5 35t-40.5 1q-37-20-77.5-31T700-240q-60 0-116 21t-104 59ZM280-494Z"/>
-                        </svg>
-                        <span style="min-width: 120px;">{{ course.category }}</span>
-                      </div>
-                    </li>
+                <div class="flex items-center">
+                  <svg class="mr-2" fill="#6366f1" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" width="24" style="min-width: 24px;">
+                    <path d="M260-320q47 0 91.5 10.5T440-278v-394q-41-24-87-36t-93-12q-36 0-71.5 7T120-692v396q35-12 69.5-18t70.5-6Zm260 42q44-21 88.5-31.5T700-320q36 0 70.5 6t69.5 18v-396q-33-14-68.5-21t-71.5-7q-47 0-93 12t-87 36v394Zm-40 118q-48-38-104-59t-116-21q-42 0-82.5 11T100-198q-21 11-40.5-1T40-234v-482q0-11 5.5-21T62-752q46-24 96-36t102-12q58 0 113.5 15T480-740q51-30 106.5-45T700-800q52 0 102 12t96 36q11 5 16.5 15t5.5 21v482q0 23-19.5 35t-40.5 1q-37-20-77.5-31T700-240q-60 0-116 21t-104 59ZM280-494Z"/>
+                  </svg>
+                  <span style="min-width: 120px;">{{ capitalizeFirstLetter(course.category) }}</span>
+                </div>
+              </li>
               <!-- Precio -->
               <li class="flex text-sm text-black p-3 border-b border-gray-100">
                 <svg class="mr-2" fill="#057500" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" width="24">
@@ -83,9 +81,7 @@
     </div>
   </div>
 </template>
-  
-  
-  
+
 <script>
 import axios from 'axios';
 
@@ -96,7 +92,6 @@ export default {
     };
   },
   async created() {
-
     const courseId = this.$route.params.id;
 
     try {
@@ -107,8 +102,10 @@ export default {
     }
   },
   methods: {
-
-  }
+    capitalizeFirstLetter(value) {
+      if (!value) return '';
+      return value.charAt(0).toUpperCase() + value.slice(1);
+    },
+  },
 };
 </script>
-  
